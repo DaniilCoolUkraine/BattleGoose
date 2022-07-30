35c9e5121c393bc016271d098d2c49963016f66b
DaniilCoolUkraine <cd0games2000@gmail.com>
2022-07-30 20:56:53 +0300

    delete unnecessary

---
+++ battkeGoose/Assets/Scripts/UIPoints.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPoints : MonoBehaviour
+{
+    public float Points { get; private set; }
+
+    [SerializeField] private Text _score;
+
+    void Update()
+    {
+        Points += Time.deltaTime;
+        _score.text = Mathf.Round(Points).ToString();
+    }
+}
