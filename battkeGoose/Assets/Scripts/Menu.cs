ebfeaf2c56d7de822cf847a318d022adfc4c79a2
DaniilCoolUkraine <cd0games2000@gmail.com>
2022-08-03 18:34:28 +0300

    added play button functionality

--- battkeGoose/Assets/Scripts/Menu.cs
+++ battkeGoose/Assets/Scripts/Menu.cs
@@ -6,14 +6,28 @@ using UnityEngine;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private GameObject _startMenu;
+    [SerializeField] private GameObject _hud;
+    [SerializeField] private GameObject _spawners;
+    [SerializeField] private GameObject _defender;
 
     public void Play()
     {
-        _startMenu.gameObject.SetActive(false);
+        SwitchGameState(true);
     }
     public void OpenMarket(){}
     public void Options(){}
     public void SwitchVibration(){}
     public void SwitchSound(){}
     public void Restart(){}
+
+    private void SwitchGameState(bool state)
+    {
+        _startMenu.gameObject.SetActive(!state);
+        _hud.gameObject.SetActive(state);
+        _spawners.gameObject.SetActive(state);
+        _defender.gameObject.GetComponent<Player>().enabled = state;
+        
+        gameObject.GetComponent<MoveBackground>().enabled = state;
+        gameObject.GetComponent<UIPoints>().enabled = state;
+    }
 }
